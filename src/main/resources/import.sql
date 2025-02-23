1de97b127171ee7170f509109f9b11e422abe2fc
Danilo Almeida <danilodesouza.a@hotmail.com>
2025-02-23 18:00:27 -0300

    Implementando o BCrypt password encoder para criptografar senha

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$eaRchPtlOdYGg9/rCTgErO21QhwF3RbGJ5rLI5CfyhhxbXYty4FAG');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$eaRchPtlOdYGg9/rCTgErO21QhwF3RbGJ5rLI5CfyhhxbXYty4FAG');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
